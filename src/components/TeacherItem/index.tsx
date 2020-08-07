ffdae4bb4d1b43d9dde2a4c925fc9c740700a0e5
Felipe Soller <soller@outlook.com>
2020-08-06 21:40:56 -0300

    Criação da busca de professores

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,34 +4,44 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
-        <header>
-            <img src="https://avatars0.githubusercontent.com/u/62728038?s=460&u=0901764d6f0e877dd57621cc0b6aa65d399a9ffb&v=4" alt="Felipe Soller"/>
-            <div>
-                <strong>Felipe Soller</strong>
-                <span>Química</span>
-            </div>
-        </header>
+            <header>
+                <img src={teacher.avatar} alt={teacher.avatar} />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
 
-        <p>
-        Entusiasta das melhores tecnologias de química avançada. 
-        <br/><br/>
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-        </p>
+            <p>{teacher.bio}</p>
 
-        <footer>
-            <p>
-                Preço/hora: 
-                <strong>R$ 80,00</strong>
-            </p>
-            <button type="button">
-                <img src={whatsappIcon} alt="Whatsapp"/>
+            <footer>
+                <p>
+                    Preço/hora:
+    <strong>R$ {teacher.cost}</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp" />
                 Entrar em contato
             </button>
-        </footer>
-    </article>
+            </footer>
+        </article>
     )
 }
 
